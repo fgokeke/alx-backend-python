d713d107364e6b3a7d681a0ce20a660e01110687
fgokeke <okekefeargod@gmail.com>
2024-04-18 21:14:30 +0000

    3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""a type-annotated function to_str"""
+
+
+def to_str(n: float) -> str:
+    """
+    Return the string representation of the given float.
+
+    Parameters:
+        n (float): The input float value.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
