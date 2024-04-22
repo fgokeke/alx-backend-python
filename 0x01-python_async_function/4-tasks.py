36b34c6fd58808bd3a34a171660151bebad1fb43
fgokeke <okekefeargod@gmail.com>
2024-04-22 16:21:12 +0000

    4. Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+This module contains asynchronous routines for waiting for random delays.
+"""
+
+import asyncio
+import random
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronously spawns wait_random n times with the specified max_delay.
+
+    Args:
+        n (int): The number of times to spawn wait_random.
+        max_delay (int): The maximum delay in seconds.
+
+    Returns:
+        List[float]: List of all the delays in ascending order.
+    """
+    delays = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
