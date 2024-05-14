f2b64c06e4e208c24964d524cb3f361958277e66
fgokeke <okekefeargod@gmail.com>
2024-05-14 04:42:50 +0000

    8 and 9 2

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -67,8 +67,7 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, expected)
 
     @parameterized_class(
-            ("org_payload", "repos_payload", "expected_repos",
-             "apache2_repos"),
+            ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
             TEST_PAYLOAD
     )
 class TestIntegrationGithubOrgClient(unittest.TestCase):
