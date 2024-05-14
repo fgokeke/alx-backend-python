05b326bb4ee06f8163b22e422bbbc18c80f33118
fgokeke <okekefeargod@gmail.com>
2024-05-14 00:38:58 +0000

    1. Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -29,3 +29,19 @@ class TestAccessNestedMap(unittest.TestCase):
         Test access_nested_map function with different inputs.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception: Exception,
+            ) -> None:
+        """
+        Test access_nested_map function raises KeyError with expected message.
+        """
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
